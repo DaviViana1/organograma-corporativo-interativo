2a592835357fc4a941853188c9061ddae3f99687
DaviViana1 <davi.n.viana1@gmail.com>
2025-07-09 15:44:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // Importe o plugin do React
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Adicione o plugin do React aqui
+      plugins: [react()], 
+      
+      // Adicione a propriedade base aqui
+      base: "/organograma-corporativo-interativo/", 
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
